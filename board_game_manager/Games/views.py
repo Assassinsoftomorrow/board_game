8f6bff4924518c3bdb84c851a8a6d66adaa475e5
Aint Mina <henry.pekkermann@hotmail.com>
2021-12-14 22:36:46 +0200

    fixes

--- board_game_manager/Games/views.py
+++ board_game_manager/Games/views.py
@@ -71,7 +71,7 @@ def edit_game(request, game_id):
             return redirect('Games:games', game_id=game.id)
 
     context = {'game': game, 'form': form}
-    return render(request, 'INSERT HTML HERE', context)
+    return render(request, 'Games/edit_games.html', context)
 
 
 @login_required
